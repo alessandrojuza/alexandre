c99c0390e57aebd92da5c2b55bd6c175d0fc9168
alessandrojuza <mascia.alessandro89@gmail.com>
2022-01-05 13:21:17 +0100

    added footer queries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Footer.scss";
+import "../queries/FooterQueries.scss";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
